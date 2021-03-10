6ba87d93bbeaf521f331e1ebd87d62b40e60801f
Ori <wiegner.ori@gmail.com>
2021-03-10 17:26:36 -0500

    hi

--- src/app/angular-js/angular-js.component.ts
+++ src/app/angular-js/angular-js.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-angular-js',
   templateUrl: './angular-js.component.html',
   styleUrls: ['./angular-js.component.css']
 })
 export class AngularJSComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    //const myWorker = new Worker("./worker.js");
+    console.log("hi")
   }
 
 }
